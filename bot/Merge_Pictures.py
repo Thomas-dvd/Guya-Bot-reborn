7110b994dd1c6e9fc11ed9c20c4d9ccd7976b9d9
Thomas-dvd <46996636+Thomas-dvd@users.noreply.github.com>
2022-11-30 22:26:25 +0100

    EDIT Merge sys

--- bot/Merge_Pictures.py
+++ bot/Merge_Pictures.py
@@ -1,15 +1,6 @@
 from PIL import Image
 
-Img1 = Image.open('img1.png')
-Img2 = Image.open('img2.png')
-Img3 = Image.open('img3.png')
-Img4 = Image.open('img4.png')
-Img5 = Image.open('img5.png')
-Img6 = Image.open('img6.png')
-Img7 = Image.open('img7.png')
-Img8 = Image.open('img8.png')
-
-vect_image=[Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8]
+#vect_image=[Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8] vect_imag.append(Img1)
 
 def merge_image(img_vector, image_size = 256):
 
@@ -18,11 +9,11 @@ def merge_image(img_vector, image_size = 256):
 
     img_vector = [elem.resize((image_size, image_size)) for elem in img_vector]
 
-    res_image = Image.new('RGB',(len(img_vector) * image_size, image_size), (255,255,255))
+    res_image = Image.new('RGB', (3 * image_size, int((len(img_vector) // 3) + 1) * image_size), (47, 49, 54))
 
     for i in range(len(img_vector)):
-        res_image.paste(img_vector[i], (i * image_size, 0))
+        res_image.paste(img_vector[i], (int(i % 3) * image_size, int(i // 3) * image_size))
 
-    res_image.save("new_image.jpg","JPEG")
+    res_image.save("images/job_picture.png")
 
-merge_image(vect_image)
+#merge_image(vect_image)
